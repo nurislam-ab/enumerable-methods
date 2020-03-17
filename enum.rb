92f051ec0c22caabba8cf6267f9e447a3897a522
nurislam-ab <abdurasulovnurislam@gmail.com>
2020-03-17 21:08:12 +0500

    Fix enum method errors

--- enum.rb
+++ enum.rb
@@ -5,7 +5,6 @@ module Enumerable
     i = 0
     range = is_a?(Range) ? to_a : self
     par = is_a?(Range) ? size : length
-
     while i < par
       if is_a?(Hash)
         yield(keys[i], self[keys[i]])
@@ -23,9 +22,9 @@ module Enumerable
     i = 0
     my_each do
       if is_a?(Hash)
-        yield(keys[i], self[keys[i]])
+        yield(self[keys[i]], keys[i])
       else
-        yield(i, self[i])
+        yield(self[i], i)
       end
       i += 1
     end
@@ -40,44 +39,47 @@ module Enumerable
   end
 
   def my_all?
-    return to_enum unless block_given?
-
-    b = true
     my_each do |i|
-      b = yield(i)
-      break unless b
+      if !arg[0].nil?
+        return false unless arg[0].equal? i
+      elsif block_given?
+        return false unless yield(i)
+      else
+        return false unless i
+      end
     end
-    b
+    true
   end
 
-  def my_any?
-    return to_enum unless block_given?
-
-    b = true
+  def my_any?(*arg)
     my_each do |i|
-      b = yield(i)
-      break if b
+      if !arg[0].nil?
+        return true if arg[0].equal? i
+      elsif block_given?
+        return true if yield(i)
+      elsif i
+        return true
+      end
     end
-    b
+    false
   end
 
-  def my_none?
-    return to_enum unless block_given?
-
-    b = true
-    my_each do |i|
-      b = !yield(i)
-      break if b
-    end
-    b
+  def my_none?(arg = nil, &block)
+    !my_any?(arg, &block)
   end
 
-  def my_count
-    c = 0
-    my_each do
-      c += 1
+  def my_count(arg = nil)
+    count = 0
+    my_each do |i|
+      if !arg.nil?
+        count += 1 if i == arg
+      elsif block_given?
+        count += 1 if yield(i)
+      else
+        count += 1
+      end
     end
-    c
+    count
   end
 
   def my_map(proc = nil)
@@ -85,29 +87,36 @@ module Enumerable
 
     arr = []
     my_each do |i|
-      arr = if block_given?
-              yield(i)
-            else
-              proc.call(i)
-            end
+      arr << block_given? ? yield(i) : proc.call(i)
     end
     arr
   end
 
-  def my_inject
-    return to_enum unless block_given?
-
+  # rubocop:todo Metrics/PerceivedComplexity
+  def my_inject(par1 = nil, par2 = nil) # rubocop:todo Metrics/CyclomaticComplexity
     arr = is_a?(Array) ? self : to_a
-    x = x if is_a?(Integer)
+
+    x = par1 if par1.is_a?(Integer)
+
+    y = par1 if par1.is_a?(Symbol) || par1.is_a?(String)
+
+    if par1.is_a?(Integer)
+      y = par2 if par2.is_a?(Symbol) || par2.is_a?(Integer)
+    end
+
     arr.my_each do |i|
-      x = x ? yield(x, i) : i
+      if block_given?
+        x = x ? yield(x, i) : i
+      elsif y
+        x = x ? x.send(y, i) : i
+      end
     end
     x
   end
+  # rubocop:enable Metrics/PerceivedComplexity
 end
 
 def multiply_els(arg)
   arg.my_inject { |m, n| m * n }
 end
-
 multiply_els([2, 4, 5])
